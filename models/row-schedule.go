feeecf272dc2282a27ef9fa06d1876fd90fc9eb1
go-numb <gonumb66@gmail.com>
2024-03-16 09:05:45 +0900

    add schedule

---
+++ models/row-schedule.go
@@ -0,0 +1,49 @@
+package models
+
+import "time"
+
+// Schedule is a struct for scheduling posts.
+type Schedule struct {
+	PostID string `csv:"-" dataframe:"post_id" firestore:"post_id,omitempty" json:"post_id,omitempty"`
+
+	IsSchedule bool `csv:"-" dataframe:"is_schedule" firestore:"is_schedule,omitempty" json:"is_schedule,omitempty"`
+
+	// DayOfMonth is the day of the month.
+	// set day
+	DayOfMonth []int `csv:"-" dataframe:"day_of_month" firestore:"day_of_month,omitempty" json:"day_of_month,omitempty"`
+	// DayOfWeek is the day of the week.
+	// set weekday
+	DayOfWeek []time.Weekday `csv:"-" dataframe:"day_of_week" firestore:"day_of_week,omitempty" json:"day_of_week,omitempty"`
+
+	Hour   []int `csv:"-" dataframe:"hour" firestore:"hour,omitempty" json:"hour,omitempty"`
+	Minute []int `csv:"-" dataframe:"minute" firestore:"minute,omitempty" json:"minute,omitempty"`
+}
+
+func (s Schedule) GetIsSchedule(t time.Time) bool {
+	if !s.IsSchedule {
+		return false
+	}
+	if len(s.DayOfMonth) == 0 && len(s.DayOfWeek) == 0 {
+		return false
+	}
+
+	// check hour
+	// 引数のDayが、ScheduleのDayに含まれているか
+	if len(s.DayOfMonth) > 0 {
+		for _, d := range s.DayOfMonth {
+			if d == t.Day() {
+				return true
+			}
+		}
+	}
+
+	if len(s.DayOfWeek) > 0 {
+		for _, d := range s.DayOfWeek {
+			if d == t.Weekday() {
+				return true
+			}
+		}
+	}
+
+	return false
+}
