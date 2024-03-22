9655456078d8697671b890cbd0bff78e28898711
go-numb <gonumb66@gmail.com>
2024-03-22 20:28:02 +0900

    add setting for account

--- models/row-setting.go
+++ models/row-setting.go
@@ -4,7 +4,7 @@ package models
 type Set struct {
 	UUID string `csv:"-" dataframe:"uuid" firestore:"uuid,omitempty" json:"uuid,omitempty"`
 
-	// 同時刻のスケジュール予約がある場合、全ての投稿を有効にするかどうか
+	// IsScheduleAllPosts 同時刻のスケジュール予約がある場合、全ての投稿を有効にするかどうか
 	// why: 複数投稿を同時刻に行う場合、全ての投稿を有効にするかどうかを設定する。年・月・週指定の同時刻投稿を行いたい。
-	IsScheduleAllPost bool `csv:"-" dataframe:"is_schedule_all_post" firestore:"is_schedule_all_post,omitempty" json:"is_schedule_all_post,omitempty"`
+	IsScheduleAllPosts bool `csv:"-" dataframe:"is_schedule_all_posts" firestore:"is_schedule_all_posts,omitempty" json:"is_schedule_all_posts,omitempty"`
 }
